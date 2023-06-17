745cfc5a13b763948e37684fdec6d5bc9e0b783b
Gerardo Rodríguez <acidbjazz@gmail.com>
2023-06-17 04:30:06 -0500

    feat: data & first query

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,18 @@
-export default function BlogPage() {
-  return <>Blog Page</>;
+import Link from "next/link";
+import { getArticles } from "@/lib/cms";
+
+export default async function BlogPage() {
+  const articles = await getArticles();
+  return (
+    <>
+      <h1>Blog</h1>
+      {articles.map((article) => {
+        return (
+          <Link href={`/blog/${article.slug}`} key={article.sys.id}>
+            <h2>{article.title}</h2>
+          </Link>
+        );
+      })}
+    </>
+  );
 }
---
+++ lib/cms.ts
@@ -0,0 +1,32 @@
+import { Article } from "./interfaces";
+
+const API_URL = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}`;
+const API_AUTH = `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`;
+
+async function graphqlClient(query: string): Promise<any> {
+  const response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: API_AUTH,
+    },
+    body: JSON.stringify({ query }),
+  });
+  return response.json();
+}
+
+export async function getArticles(): Promise<Article[]> {
+  const query = `{
+    articleCollection {
+      items {
+        sys { id }
+        title
+        slug
+        cover { url }
+        body
+      }
+    }
+  }`;
+  const { data } = await graphqlClient(query);
+  return data.articleCollection.items;
+}
